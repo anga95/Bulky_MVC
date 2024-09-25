b0785b3d298e80097afea03b0087fb8adc4f5bdb
Andreas Gabrielsson <100436631+anga95@users.noreply.github.com>
2024-09-25 08:51:56 +0200

    526652

---
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using Bulky.Models.ViewModels;
+using Bylky.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                    includeProperties: "Product")
+            };
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
--- Bylky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bylky.DataAccess/Repository/IRepository/IRepository.cs
@@ -4,7 +4,7 @@ namespace Bylky.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, Boolean>>? filter = null, string? includeProperties = null);
         T Get(Expression<Func<T, Boolean>> filter, string? includeProperties = null, bool tracked = false);
         void Add(T entity);
         void Remove(T entity);
